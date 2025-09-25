433d8941820a12033ac8f4f728cdc397a2ecdc7b
thpauken <thpauken@hotmail.com>
2025-09-25 20:47:59 +0000

    Starting fresh

--- backend/src/db/models/user.js
+++
@@ -1,6 +0,0 @@
-import mongoose, { Schema } from 'mongoose'
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-})
-export const User = mongoose.model('user', userSchema)
--- backend/src/middleware/jwt.js
+++
@@ -1,5 +0,0 @@
-import { expressjwt } from 'express-jwt'
-export const requireAuth = expressjwt({
-  secret: () => process.env.JWT_SECRET,
-  algorithms: ['HS256'],
-})
